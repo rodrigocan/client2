c7f4856815810d6657e7ba796f9ebddd23302017
Rodrigo Cândido <rodrigocan@gmail.com>
2022-02-06 21:09:51 -0300

    149. Logo Parte 6: Criando uma outra prop da logo usando o TDD

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -11,10 +11,17 @@ describe('<Logo />', () => {
     })
   })
 
-  it('should render a black label when color is passed', () => {
-    renderWithTheme(<Logo color="black" />)
+  it('should render a normal logo when size is default', () => {
+    renderWithTheme(<Logo />)
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
-      color: '#030517'
+      width: '11rem'
+    })
+  })
+
+  it('should render a bigger logo', () => {
+    renderWithTheme(<Logo size="large" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '20rem'
     })
   })
 })
